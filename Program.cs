0f12d2a8b674ef029c67d2b56ad8e7696927699e
Shiranaiwayo <susanne.j.r@me.com>
2026-09-08 06:42:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercises1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, shit!");
+
+        }
+    }
+}
